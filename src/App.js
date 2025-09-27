366431c0941d668cefcea5e8dc43ff453a00ab8c
MatiasBernardis <matias.bernardis@hotmail.com>
2025-09-27 00:02:42 -0300

    mejorando estilos generales

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Buscador from './components/buscador';
 import Resultados from './components/resultados';
+import logo from './img/centraldedeudores.png';
 
 function App() {
 
@@ -12,7 +13,9 @@ function App() {
       <h1>🇦🇷Bienvenidos a Central de Deudores del BCRA🇦🇷</h1>
       <Buscador response={response} setResponse={setResponse} />
       <Resultados response={response} />
-      <footer>Página desarrollada por <a href='https://github.com/bernardisApps'>Bernardis Apps</a></footer>
+      <footer>Página desarrollada por <a href='https://github.com/bernardisApps'>Bernardis Apps</a>
+        <img className='logo' src={logo} alt='Logo de central de deudores' ></img>
+      </footer>
     </div>
   );
 }
